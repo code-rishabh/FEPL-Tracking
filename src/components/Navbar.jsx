e0ee00d3b83e4bb78c2836d6769c55212dd8b34f
Rishabh Sharma <r08s2001@gmail.com>
2024-08-01 14:35:57 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-
+import feplLogo from '../assets/FEPL-Website-Logo.png'
 
 const Navbar = () => {
   return (
     <div className="container m-auto bg-gradient-to-r from-slate-100 to-red-600 flex justify-around gap-[40vw] h-16 text-lg items-center fixed w-full z-20 shadow-md">
-      <span><img className="h-14" src="/src/assets/FEPL-Website-Logo.png" alt="ForeExcel" /></span>
+      <span><img className="h-14" src={feplLogo} alt="ForeExcel" /></span>
       <div className="">
         <ul className="flex gap-5 text-white text-sm">
           <li>Find a Location</li>
